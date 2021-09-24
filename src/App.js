21a3ee3cebb0bd480c9d524497992315d4794c18
Arun Kaushik <46048646+incrediblenura@users.noreply.github.com>
2021-09-24 17:21:11 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import './styles/App.css';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { Skills } from './components/Skills';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Skills/>
+      <Main/>
+    </div>
+  );
+}
 
+export default App;
